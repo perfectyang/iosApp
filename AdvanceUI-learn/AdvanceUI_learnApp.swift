9c7a3f93aec6e2799b5a354dd83a7bd52ec9084b
yangguowei <yangguowei@joyy.com>
2022-02-15 10:47:43 +0800

    Initial Commit

---
+++ AdvanceUI-learn/AdvanceUI_learnApp.swift
@@ -0,0 +1,17 @@
+//
+//  AdvanceUI_learnApp.swift
+//  AdvanceUI-learn
+//
+//  Created by perfectyang on 2022/2/15.
+//
+
+import SwiftUI
+
+@main
+struct AdvanceUI_learnApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AdvanceUI-learn/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AdvanceUI-learn
+//
+//  Created by perfectyang on 2022/2/15.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
